a569018146f72222a9352feba78122b6b7b8d09f
Sai Swaroop <saiswaroop874@gmail.com>
2023-08-19 14:44:12 +0530

    c

--- mongodb/app.js
+++ mongodb/app.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/test');
 
-const Cat = mongoose.model('Cat', { name: String });
+const Cat = mongoose.model('Cat', { name: String, isBig: Boolean });
 
-const kitty = new Cat({ name: 'tom' });
+const kitty = new Cat({ name: 'tom', isBig: 'false' });
 kitty.save()
       .then(() => console.log('✅ Cat Saved'))
       .catch( err => console.log('❌ Error'))
+      // .finally( () => mongoose.disconnect())
+
+Cat.find((err, cats) => {
+      if(err) return
+      console.log(cats)
+})
